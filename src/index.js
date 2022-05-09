2fa23e63209551a03792eb0777030c7e1a0c2c18
jieun-c <flffl1231@naver.com>
2022-05-09 21:52:44 +0900

    State Mutation

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
 // NEVER! MUTATE STATE!
+// store를 수정하기 위한 방법은 action을 보내는 방법뿐이다.
+// mutate 하는것이 아니라 새로운 objects(state)를 리턴한다.
+// 공식문서에서는 reducer 안에서 Date.now()를 쓰지 않길 권장하고 있다.
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text, id: action.id }];
     case DELETE_TODO:
       return [];
     default:
@@ -20,12 +23,13 @@ const reducer = (state = [], action) => {
 };
 
 const store = createStore(reducer);
+store.subscribe(() => console.log(store.getState()));
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  store.dispatch({ type: ADD_TODO, text: toDo, id: Date.now() });
 };
 
 form.addEventListener("submit", onSubmit);
