561ed6df56a2aa32639535e9f1720a62dad40982
jieun-c <flffl1231@naver.com>
2022-05-09 21:04:12 +0900

    Recap Refactor

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-// 초기값
 number.innerText = 0;
 
+// 2. 안전하게 const 로 변경
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+// 1. if문을 switch 문으로 변경
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -23,14 +28,13 @@ const onChange = () => {
   number.innerText = countStore.getState();
 };
 
-// subscribe 는 store 안에 있는 변화를 알 수 있게함
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 plus.addEventListener("click", handleAdd);
