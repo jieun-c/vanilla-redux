8167565af146e5979fbfe95a4aee64eadea3f17f
jieun-c <flffl1231@naver.com>
2022-05-10 13:59:47 +0900

    Detail

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,7 +1,36 @@
-import React from "react";
+import { connect } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { actionCreators } from "../store/store";
 
-const Detail = () => {
-  return <></>;
+const Detail = ({ toDos, onBtnClick }) => {
+  const id = useParams().id;
+  const toDo = toDos.find((toDo) => toDo.id === parseInt(id));
+  const navigate = useNavigate();
+
+  const handleDel = () => {
+    onBtnClick(id);
+    navigate("/");
+  };
+
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {toDo?.id}</h5>
+      <button onClick={handleDel}>DEL</button>
+    </>
+  );
 };
 
-export default Detail;
+function mapStateToProps(state) {
+  return {
+    toDos: state,
+  };
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onBtnClick: (id) => dispatch(actionCreators.deleteToDo(id)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
